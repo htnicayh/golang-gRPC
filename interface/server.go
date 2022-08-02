bbab7590e6cd3cf7d2a5bdb4e033c8c3cd95ed56
hyacinth <hieu.b.dao@vntrip.vn>
2022-08-02 11:27:43 +0700

    add interface

---
+++ interface/server.go
@@ -0,0 +1,101 @@
+package main
+
+import "fmt"
+
+// type Human struct {
+// 	name  string
+// 	age   int
+// 	phone string
+// }
+
+// type Student struct {
+// 	Human
+// 	school string
+// 	loan   float32
+// }
+
+// type Employee struct {
+// 	Human
+// 	company string
+// 	money   float32
+// }
+
+// // Define Interface
+// type FirstInterface interface {
+// 	SayHi()
+// 	Sing(lyrics string)
+// }
+
+// func (h Human) SayHi() {
+// 	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
+// }
+
+// func (h Human) Sing(lyrics string) {
+// 	fmt.Println("La la la la...", lyrics)
+// }
+
+// func (e Employee) SayHi() {
+// 	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name, e.company, e.phone)
+// }
+
+type Writer interface {
+	Write([]byte) (int, error)
+}
+
+type ConsoleWriter struct {
+	message string
+	index   int
+}
+
+func (cw *ConsoleWriter) Write(data []byte) (int, error) {
+	var (
+		message int
+		err     error
+	)
+
+	if cw.index == 0 {
+		message = len(cw.message)
+		err = nil
+	} else {
+		message = 0
+		err = fmt.Errorf("ConsoleWriter: Write error")
+	}
+
+	return message, err
+}
+
+func main() {
+	var w Writer = &ConsoleWriter{"Hello, world", 3}
+	w.Write([]byte("Hello, World!"))
+
+	// mike := Student{Human{"Mike", 25, "222-222-XXX"}, "MIT", 0.00}
+	// // paul := Student{Human{"Paul", 26, "111-222-XXX"}, "Harvard", 100}
+	// // sam := Employee{Human{"Sam", 36, "444-222-XXX"}, "Golang Inc.", 1000}
+	// tom := Employee{Human{"Tom", 36, "444-222-XXX"}, "Things Ltd.", 5000}
+
+	// // define interface i
+	// var i FirstInterface
+
+	// //i can store Student
+	// i = mike
+	// fmt.Println("This is Mike, a Student:")
+	// i.SayHi()
+	// i.Sing("November rain")
+
+	// //i can store Employee
+	// i = tom
+	// fmt.Println("This is Tom, an Employee:")
+	// i.SayHi()
+	// i.Sing("Born to be wild")
+
+	// // slice of FirstInterface
+	// fmt.Println("Let's use a slice of FirstInterface and see what happens")
+	// x := make([]FirstInterface, 3)
+	// // these three elements are different types but they all implemented interface Men
+	// x[0], x[1], x[2] = paul, sam, mike
+
+	// for _, value := range x {
+	// 	value.SayHi()
+	// }
+
+}
